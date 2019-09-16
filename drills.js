df7f7bf005ead4e3e6462cc868a4d098bc9eb107
Alex Bannow <rbannal@gmail.com>
2019-09-16 15:32:26 -0400

    added beyond drill

--- drills.js
+++ drills.js
@@ -2,4 +2,20 @@
 function jediName(firstName, lastName){
   return lastName.slice(0,3) + firstName.slice(0,2);
 }
-console.log(jediName('Beyonce', 'Knowles'));
+//console.log(jediName('Beyonce', 'Knowles'));
+
+
+function beyond(num){
+  if(num === 0){
+    console.log('Staying Home');
+  }
+  else if(!isFinite(num)){
+    console.log('And Beyond');
+  }
+  else if(num < 0){
+    console.log('To negative infinity');
+  }
+  else {
+    console.log('To infinity');
+  }
+}
